69090cef21617b99b0f0506eb735bd10298afd79
gejeduck <47668701+gejeduck@users.noreply.github.com>
2025-03-19 09:53:14 -0400

    update rev for pyth integration

--- packages/layerzero-v2/aptos/contracts/run_tests.sh
+++ packages/layerzero-v2/aptos/contracts/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Find all directories containing Move.toml and run the command
-find . -name 'Move.toml' -execdir sh -c 'echo "Running tests in $(pwd)" && aptos move test --dev' \;
+find . -name 'Move.toml' -execdir sh -c 'echo "Running tests in $(pwd)" && aptos move test --dev --move-2' \;
